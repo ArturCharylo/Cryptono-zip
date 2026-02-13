54f7d246e86646c6f81e02b8dc0ec41ba929317c
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-02-13 13:09:33 +0100

    Implemented small fixes and added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,7 @@
-use std::{collections::{BinaryHeap, HashMap}};
+use std::collections::{BinaryHeap, HashMap};
+use std::cmp::Ordering;
 
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Node {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.freq.cmp(&self.freq)
-    }
-}
-
-#[derive(Debug, Eq, PartialEq)] 
+#[derive(Debug, Eq, PartialEq)]
 pub struct Node {
     pub freq: u32,
     pub ch: Option<char>,
@@ -20,8 +9,19 @@ pub struct Node {
     pub right: Option<Box<Node>>,
 }
 
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.freq.cmp(&self.freq)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 impl Node {
-    // List node constructor
     pub fn new_leaf(freq: u32, ch: char) -> Node {
         Node {
             freq,
@@ -31,7 +31,6 @@ impl Node {
         }
     }
 
-    // Inner node constructuor
     pub fn new_internal(freq: u32, left: Node, right: Node) -> Node {
         Node {
             freq,
@@ -42,35 +41,45 @@ impl Node {
     }
 }
 
-pub fn build_tree(freq_map: HashMap<char, u32>) -> Option<Node>{
+pub fn calculate_freq(input: &str) -> HashMap<char, u32> {
+    let mut freq_map = HashMap::new();
+    for chr in input.chars() {
+        *freq_map.entry(chr).or_insert(0) += 1;
+    }
+    freq_map
+}
+
+pub fn build_tree(freq_map: &HashMap<char, u32>) -> Option<Node> {
     let mut heap = BinaryHeap::new();
 
-    for (chr, freq) in freq_map{
+    for (&chr, &freq) in freq_map {
         heap.push(Node::new_leaf(freq, chr));
+    }
+
+    if heap.is_empty() {
+        return None;
     }
 
     while heap.len() > 1 {
         let left_node = heap.pop().unwrap();
         let right_node = heap.pop().unwrap();
 
-        heap.push(Node::new_internal(left_node.freq + right_node.freq, left_node, right_node));
+        let new_freq = left_node.freq + right_node.freq;
+        heap.push(Node::new_internal(new_freq, left_node, right_node));
     }
-    heap.pop()
-}
 
-pub fn calculat_freq(input: &str) -> std::collections::HashMap<char, u32> {
-    let mut freq_map: HashMap<char, u32> = std::collections::HashMap::new();
-    for chr in input.chars() {
-        let count = freq_map.entry(chr).or_insert(0);
-        *count += 1;
-    }
-    freq_map
+    heap.pop()
 }
 
 fn generate_codes(node: &Node, current_code: String, codes: &mut HashMap<char, String>) {
     if let Some(chr) = node.ch {
-        codes.insert(chr, current_code);
-        return; 
+        let code_to_insert = if current_code.is_empty() {
+            "0".to_string()
+        } else {
+            current_code
+        };
+        codes.insert(chr, code_to_insert);
+        return;
     }
 
     if let Some(ref left) = node.left {
@@ -87,23 +96,40 @@ fn generate_codes(node: &Node, current_code: String, codes: &mut HashMap<char, S
 }
 
 pub fn compress_data(input: &str) -> String {
-    let freq_map = calculat_freq(input);
-    let root = build_tree(freq_map);
+    let freq_map = calculate_freq(input);
+    let root = build_tree(&freq_map);
 
     let mut codes = HashMap::new();
+
     if let Some(tree_root) = root {
         generate_codes(&tree_root, String::new(), &mut codes);
     }
 
     let mut compressed = String::new();
-    
     for chr in input.chars() {
-        compressed.push_str(codes.get(&chr).expect("No Huffman code found"));
+        if let Some(code) = codes.get(&chr) {
+            compressed.push_str(code);
+        }
     }
+
     compressed
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_huffman_simple() {
+        let input = "AAABBC";
+        let compressed = compress_data(input);
+        assert!(!compressed.is_empty());
+    }
+
+    #[test]
+    fn test_single_char() {
+        let input = "AAAA";
+        let compressed = compress_data(input);
+        assert_eq!(compressed, "0000");
+    }
 }
